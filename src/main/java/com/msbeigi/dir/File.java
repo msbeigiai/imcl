d64aa07d62797ba929d013629ceb8fd25e1e5dfc
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-09 19:07:34 +0200

    Update File.java

--- src/main/java/com/msbeigi/dir/File.java
+++ src/main/java/com/msbeigi/dir/File.java
@@ -1,5 +1,10 @@
 package com.msbeigi.dir;
 
+import com.msbeigi.extention.FileExtensionException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class File extends DirOps {
 
     public File(String name) {
@@ -9,5 +14,15 @@ public class File extends DirOps {
     @Override
     protected void create() {
         System.out.println("Create file");
+    }
+
+    @Override
+    public void opsCreationExtension(String fileName) throws FileExtensionException {
+        final Pattern pattern = Pattern.compile("[A-Za-z]+\\.[A-Za-z]+", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(fileName);
+        if (!matcher.matches()) {
+            throw new FileExtensionException();
+        }
+        this.setName(fileName);
     }
 }
