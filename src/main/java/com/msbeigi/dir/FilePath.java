d80df8986212f84dd764d799406415d210840dbf
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-10 23:08:43 +0200

    Update FilePath.java

--- src/main/java/com/msbeigi/dir/FilePath.java
+++ src/main/java/com/msbeigi/dir/FilePath.java
@@ -1,6 +1,12 @@
 package com.msbeigi.dir;
 
 public class FilePath extends PathOps {
+    DirOps dirOps;
+
+    public FilePath(DirOps dirOps) {
+        this.dirOps = dirOps;
+    }
+
     public static void appendCommand(DirOps dirOps) {
         PATH.add(dirOps.getName());
     }
