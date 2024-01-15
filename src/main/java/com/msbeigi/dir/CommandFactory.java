7f5e417faa52970b5f1fc68f189d1250949f69be
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-15 19:50:22 +0200

    Update CommandFactory.java

--- src/main/java/com/msbeigi/dir/CommandFactory.java
+++ src/main/java/com/msbeigi/dir/CommandFactory.java
@@ -1,5 +1,6 @@
 package com.msbeigi.dir;
 
+import com.msbeigi.exceptions.CommandNotAssignedException;
 import com.msbeigi.exceptions.FileExtensionException;
 import com.msbeigi.exceptions.FileOrDirNotExistException;
 
@@ -14,39 +15,45 @@ public class CommandFactory extends CommandOps implements Command {
     }
 
     @Override
-    public void execute() {
+    public void execute() throws CommandNotAssignedException {
         StringTokenizer stringTokenizer = new StringTokenizer(userCommand);
-        String command = stringTokenizer.nextToken();
+        String command = stringTokenizer.hasMoreTokens() ? stringTokenizer.nextToken() : "";
         String name = stringTokenizer.hasMoreTokens() ? stringTokenizer.nextToken() : "";
-        switch (command) {
-            case MKDIR -> {
-                Directory directory = new Directory(name);
-                DirPath.appendCommand(directory);
-                DirPath.currentPath();
-            }
 
-            case TOUCH -> {
-                try {
-                    File file = new File(name);
-                    file.opsCreationExtension(name);
-                    FilePath.appendCommand(file);
-                } catch (FileExtensionException e) {
-                    System.out.println(e.getMessage());
+        if (command != null && !command.isEmpty()) {
+            switch (command) {
+                case MKDIR -> {
+                    Directory directory = new Directory(name);
+                    DirPath.appendCommand(directory);
+                    DirPath.currentPath();
                 }
-                DirPath.currentPath();
-            }
 
-            case DELETE -> {
-                try {
-                    DeleteOps.deleteExecute(name);
-                } catch (FileOrDirNotExistException e) {
-                    System.out.println(e.getMessage());
+                case TOUCH -> {
+                    try {
+                        File file = new File(name);
+                        file.opsCreationExtension(name);
+                        FilePath.appendCommand(file);
+                    } catch (FileExtensionException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    DirPath.currentPath();
                 }
-                DirPath.currentPath();
-            }
 
-            case LS -> {
-                DirPath.currentPath();
+                case DELETE -> {
+                    try {
+                        DeleteOps.deleteExecute(name);
+                    } catch (FileOrDirNotExistException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    DirPath.currentPath();
+                }
+
+                case LS -> {
+                    DirPath.currentPath();
+                }
+
+                default -> throw new CommandNotAssignedException(command);
+
             }
         }
     }
