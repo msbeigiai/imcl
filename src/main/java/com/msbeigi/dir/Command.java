2d15ab8b94ceb396003bee98477ca260facb473e
Mohsen Sadeghbeigi <msbeigi83@gmail.com>
2024-01-15 19:50:14 +0200

    Update Command.java

--- src/main/java/com/msbeigi/dir/Command.java
+++ src/main/java/com/msbeigi/dir/Command.java
@@ -1,7 +1,8 @@
 package com.msbeigi.dir;
 
+import com.msbeigi.exceptions.CommandNotAssignedException;
 import com.msbeigi.exceptions.FileExtensionException;
 
 public interface Command {
-    void execute();
+    void execute() throws CommandNotAssignedException;
 }
